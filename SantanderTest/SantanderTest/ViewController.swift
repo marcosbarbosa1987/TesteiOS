13b147ac64ba6eb47ed0f4b37fb2f3b461a93a42
Marcos Barbosa <marcosbarbosa1987@live.com>
2018-07-20 09:17:55 -0300

    Create the project Add Alamofire

---
+++ SantanderTest/SantanderTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SantanderTest
+//
+//  Created by Marcos Barbosa on 20/07/2018.
+//  Copyright © 2018 n/a. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
